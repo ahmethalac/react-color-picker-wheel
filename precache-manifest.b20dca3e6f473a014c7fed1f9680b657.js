563e6cb238b0ed1b150fe8013751435bdfc0888e
Ahmet Feyzi Halaç <ahmetfeyzihalac@gmail.com>
2020-09-09 22:11:56 +0300

    Updates

---
+++ precache-manifest.b20dca3e6f473a014c7fed1f9680b657.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ccefd93404d8c7781b379540c391abd",
+    "url": "/react-color-picker-wheel/index.html"
+  },
+  {
+    "revision": "47709dadc19f91f861e3",
+    "url": "/react-color-picker-wheel/static/css/main.2e937c01.chunk.css"
+  },
+  {
+    "revision": "b245f3cfa5256081a5b3",
+    "url": "/react-color-picker-wheel/static/js/2.487d968f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-color-picker-wheel/static/js/2.487d968f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47709dadc19f91f861e3",
+    "url": "/react-color-picker-wheel/static/js/main.f4bcd834.chunk.js"
+  },
+  {
+    "revision": "d657cc76ce9308986bc4",
+    "url": "/react-color-picker-wheel/static/js/runtime-main.cac68ca2.js"
+  }
+]);
